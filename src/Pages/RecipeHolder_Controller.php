06e942fb2d6ba3b2835bf8bece465a5dafc69785
Shane Watters <shane.watters.nz@gmail.com>
2020-11-30 15:24:32 +1300

    MAJOR: upgrade to new version of Silverstripe - step: Change Controller::init function to protected

--- src/Pages/RecipeHolder_Controller.php
+++ src/Pages/RecipeHolder_Controller.php
@@ -22,7 +22,15 @@ class RecipeHolder_Controller extends Blog_Controller
         return $this->renderAjaxRecipes();
     }
 
-    public function init()
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD:     public function init() (ignore case)
+  * NEW:     protected function init() (COMPLEX)
+  * EXP: Controller init functions are now protected  please check that is a controller.
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    protected function init()
     {
         parent::init();
 
