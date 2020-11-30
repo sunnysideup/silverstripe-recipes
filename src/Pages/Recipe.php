2e52c3e66c9b24dd56a3fe26200a51ebc16025fb
Shane Watters <shane.watters.nz@gmail.com>
2020-11-30 15:24:24 +1300

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- src/Pages/Recipe.php
+++ src/Pages/Recipe.php
@@ -18,6 +18,20 @@ class Recipe extends BlogPost
      *
      * @var array
      */
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'Recipe';
+
     private static $db = [
         'FeaturedVideo' => 'Varchar(255)',
         'FeatureOnHomePage' => 'Boolean',
--- src/Pages/RecipeHolder.php
+++ src/Pages/RecipeHolder.php
@@ -7,6 +7,20 @@
 class RecipeHolder extends Blog
 {
 
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'RecipeHolder';
+
     private static $db = [
         'RecipesSideBarVideo' => 'Varchar(255)',
     ];
